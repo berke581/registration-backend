2db63e862cb14046ca5f16253f22cd56935fbba6
Berke YONCACI <berke5sekiz1@gmail.com>
2022-03-14 16:41:05 +0300

    initial commit

---
+++ src/test/java/com/berkeyoncaci/registrationapi/RegistrationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkeyoncaci.registrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
